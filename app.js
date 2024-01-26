c093bab7f8e62c575f71ec29d3506e0aea7a6130
mihir7121 <mihir.nikam1@gmail.com>
2024-01-26 18:17:52 -0500

    added user auth routes

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ var express         = require("express"),
     methodOverride  = require("method-override"),
     flash           = require("connect-flash"),
     dotenv          = require('dotenv'),
+    passport        = require("passport"),
+    LocalStrategy   = require("passport-local"),
+    User            = require("./models/user"),
     app             = express();
 
+var indexRoutes     = require("./routes/index");
 dotenv.config();
-// var user = process.env.USER;
-// var password = process.env.PASSWORD;
-// var mongo_url = "mongodb+srv://"+user+":"+password+"@cluster0.2ecwzmd.mongodb.net/?retryWrites=true&w=majority";
 mongoose.connect(`mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.2ecwzmd.mongodb.net/?retryWrites=true&w=majority`,
 ).then(() => {
     console.log('Connected to DB!');
@@ -24,9 +25,26 @@ app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 app.use(flash());
 
-app.get("",function(req,res){
-    res.render("landing"); 
+//PASSPORT CONIGURATION
+app.use(require("express-session")({
+    secret:process.env.SECRET,
+    resave:false,
+    saveUninitialized:false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req,res,next){
+    res.locals.currentUser = req.user;
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
 });
+
+app.use("/", indexRoutes);
 
 app.listen(process.env.PORT,function(){
     console.log(`MeloDen server has started! http://localhost:${process.env.PORT}`);
---
+++ routes/index.js
@@ -0,0 +1,51 @@
+var express = require("express");
+var router  = express.Router();
+var passport = require("passport");
+var User = require("../models/user");
+
+router.get("/",function(req,res){
+    res.render("landing");
+});
+//================
+//  AUTH ROUTES
+//================
+
+router.get("/register",function(req,res){
+    res.render("register");
+});
+
+router.post("/register",function(req,res){
+    var newUser = new User({username:req.body.username});
+    User.register(newUser, req.body.password, function(err,user){
+        if(err){ 
+                return res.render("register", {"error": err.message});
+        } 
+        passport.authenticate("local")(req,res,function(){
+            req.flash("success", "Successfully Signed Up! Nice to meet you, " + req.body.username);
+            res.redirect("/");
+        });
+    });
+});
+
+router.get("/login",function(req,res){
+   res.render("login"); 
+});
+
+router.post("/login", passport.authenticate("local",
+        {
+            successRedirect:"/",
+            failureRedirect:"/login",
+            failureFlash: true
+        }),function(req,res){
+});
+
+router.get("/logout",function(req,res){
+    req.flash("success", "Successfully logged you out!");
+    req.logout();
+    res.redirect("/");
+});
+
+router.get("/about",function(req,res){
+    res.render("about");
+});
+module.exports = router;
